772bc6729d6405f46e7c0ee22920ea7de5bdfbbf
Gary Burgess <gary.burgess@gmail.com>
2018-11-30 17:57:13 +0000

    Add injection tests

--- test/Main.purs
+++ test/Main.purs
@@ -2,7 +2,10 @@ module Test.Main where
 
 import Prelude
 
+import Data.Const (Const)
 import Data.Functor.Compose (Compose(..))
+import Data.Functor.Coproduct (Coproduct, left, right)
+import Data.Functor.Coproduct.Inject (inj)
 import Data.Identity (Identity(..))
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
@@ -10,6 +13,11 @@ import Test.Assert (assertEqual)
 
 main :: Effect Unit
 main = do
+  testComposeOrdering
+  testInjections
+
+testComposeOrdering :: Effect Unit
+testComposeOrdering = do
   assertEqual
     { expected: Compose (Identity (Just true))
     , actual: Compose (Identity (Just true))
@@ -17,4 +25,19 @@ main = do
   assertEqual
     { expected: Compose (Identity (Just true)) > Compose (Identity (Just false))
     , actual: true
+    }
+
+testInjections :: Effect Unit
+testInjections = do
+  assertEqual
+    { expected: Identity unit
+    , actual: inj (Identity unit)
+    }
+  assertEqual
+    { expected: left (Identity unit) :: Coproduct Identity (Const Void) Unit
+    , actual: inj (Identity unit)
+    }
+  assertEqual
+    { expected: right (Identity unit) :: Coproduct (Const Void) Identity Unit
+    , actual: inj (Identity unit)
     }
