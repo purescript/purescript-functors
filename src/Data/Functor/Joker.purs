679c22ac1450a08924c11e2858ffeb7493d8b917
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-12-22 07:51:51 -0800

    Merge profunctor Joker into bifunctor Joker

--- src/Data/Functor/Joker.purs
+++ src/Data/Functor/Joker.purs
@@ -9,6 +9,7 @@ import Data.Bifunctor (class Bifunctor)
 import Data.Newtype (class Newtype)
 
 -- | Make a `Functor` over the second argument of a `Bifunctor`
+newtype Joker :: forall k1 k2. (k1 -> Type) -> k2 -> k1 -> Type
 newtype Joker g a b = Joker (g b)
 
 derive instance newtypeJoker :: Newtype (Joker g a b) _
@@ -23,6 +24,17 @@ instance showJoker :: Show (g b) => Show (Joker g a b) where
 instance functorJoker :: Functor g => Functor (Joker g a) where
   map g (Joker a) = Joker (map g a)
 
+instance applyJoker :: Apply f => Apply (Joker f a) where
+  apply (Joker f) (Joker g) = Joker $ apply f g
+
+instance applicativeJoker :: Applicative f => Applicative (Joker f a) where
+  pure = Joker <<< pure
+
+instance bindJoker :: Bind f => Bind (Joker f a) where
+  bind (Joker ma) amb = Joker $ ma >>= (amb >>> un Joker)
+
+instance monadJoker :: Monad m => Monad (Joker m a)
+
 instance bifunctorJoker :: Functor g => Bifunctor (Joker g) where
   bimap _ g (Joker a) = Joker (map g a)
 
@@ -31,3 +43,6 @@ instance biapplyJoker :: Apply g => Biapply (Joker g) where
 
 instance biapplicativeJoker :: Applicative g => Biapplicative (Joker g) where
   bipure _ b = Joker (pure b)
+
+hoistJoker :: forall f g a b. (f ~> g) -> Joker f a b -> Joker g a b
+hoistJoker f (Joker a) = Joker (f a)
