ecba4c3c1c6e6ec5809fc98ff34ffb91a2ce2846
Jordan Martinez <jordanalex.martinez@gmail.com>
2021-01-25 12:29:09 -0800

    Specialize FunctorRight's first type parameter to kind Type

--- src/Data/Functor/FunctorRight.purs
+++ src/Data/Functor/FunctorRight.purs
@@ -2,6 +2,6 @@ module Data.Functor.FunctorRight where
 
 -- | Same as `Functor` but works on types that take two type parameters
 -- | instead of just one.
-class FunctorRight :: forall k. (k -> Type -> Type) -> Constraint
+class FunctorRight :: (Type -> Type -> Type) -> Constraint
 class FunctorRight f where
   rmap :: forall a b c. (b -> c) -> f a b -> f a c
